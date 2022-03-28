24d6dff9c1c050e788f281ef85d515b6de29957e
auliakhansaaa <38196380+auliakhansaaa@users.noreply.github.com>
2022-03-28 11:46:18 +0700

    versi 2

--- jsmain.js
+++ jsmain.js
@@ -11,6 +11,30 @@ class Game {
         this.controlKeterangan = document.querySelector(
             ".rec .keterangan"
         );
+
+        this.controlGuntingBack = document.querySelector(
+            ".gunting-back"
+        );
+        this.controlBatuBack = document.querySelector(
+            ".batu-back"
+        );
+        this.controlKertasBack = document.querySelector(
+            ".kertas-back"
+        );
+
+        this.controlGunting2Back = document.querySelector(
+            ".gunting2-back"
+        );
+        this.controlBatu2Back = document.querySelector(
+            ".batu2-back"
+        );
+        this.controlKertas2Back = document.querySelector(
+            ".kertas2-back"
+        );
+
+        this.controlKotakKeterangan = document.querySelector(
+            ".rec"
+        );
     
         //   this.controlAngka = document.querySelector("#game-control .angka");
         //   this.controlAngka.textContent = 0;
@@ -19,6 +43,7 @@ class Game {
         this.controlButton = document.querySelector(".batu-1 button");
         console.log(this.controlButton);
         this.controlButton.addEventListener("click", () => {
+            this.controlBatuBack.style.display ="block";
             console.log('test');
             this.batu();
         });
@@ -26,6 +51,7 @@ class Game {
         this.controlButton2 = document.querySelector(".kertas-1 button");
         console.log(this.controlButton2);
         this.controlButton2.addEventListener("click", () => {
+            this.controlKertasBack.style.display ="block";
         console.log('test');
         this.kertas();
         });
@@ -33,6 +59,7 @@ class Game {
         this.controlButton3 = document.querySelector(".gunting-1 button");
         console.log(this.controlButton3);
         this.controlButton3.addEventListener("click", () => {
+            this.controlGuntingBack.style.display ="block";
             console.log('test');
             this.gunting();
         });
@@ -57,16 +84,64 @@ class Game {
   
         // this.controlAngka.style.color = null;
   
-        this.com(angka,nilai);
+        if(angka===nilai){
+            this.controlKotakKeterangan.style.display="block";
+            this.controlKeterangan.style.display ="block";
+            this.controlBatu2Back.style.display ="block";
+          console.log(angka);
+          console.log(nilai);
+          this.controlKeterangan.textContent="DRAW";
+          } 
+          
+          else if(angka==2){
+              this.controlKotakKeterangan.style.display="block";
+              this.controlKeterangan.style.display ="block";
+              this.controlKertas2Back.style.display ="block";
+          console.log(angka);
+          console.log(nilai);
+          this.controlKeterangan.textContent="COM WON";
+  
+          }else if(angka==3){
+              this.controlKotakKeterangan.style.display="block";
+              this.controlKeterangan.style.display ="block";
+              this.controlGunting2Back.style.display ="block";
+          console.log(angka);
+          console.log(nilai);
+          this.controlKeterangan.textContent="PLAYER WON";
+  
+          }
         
       }, 2000); // 500 = 2 detik
-
+   
     }
 
-    
     gunting() {
     
         let nilai = 2;
+        let angka;
+        const interval = setInterval(() => {
+          angka = Math.ceil(Math.random() * 3); // Random 1-5
+          
+          
+          // this.controlAngka.textContent = angka;
+        }, 50); // 50 = 0.05 detik
+    
+        // Interval berhenti di detik ke-2
+        setTimeout(() => {
+          clearInterval(interval);
+    
+          // this.controlAngka.style.color = null;
+    
+          this.com(angka,nilai);
+          
+        }, 2000); // 500 = 2 detik
+  
+      }
+
+    
+    kertas() {
+    
+        let nilai = 3;
         let angka;
         const interval = setInterval(() => {
           angka = Math.ceil(Math.random() * 3); // Random 1-3
@@ -86,18 +161,23 @@ class Game {
   
     com(angka,nilai){
         if(angka===nilai){
-        //   this.controlKeterangan.style.display = null;
+          this.controlKotakKeterangan.style.display="block";
+          this.controlKeterangan.style.display ="block";
         console.log(angka);
         console.log(nilai);
         this.controlKeterangan.textContent="player won";
         } 
         
         else if(angka > nilai){
+            this.controlKotakKeterangan.style.display="block";
+            this.controlKeterangan.style.display ="block";
         console.log(angka);
         console.log(nilai);
         this.controlKeterangan.textContent="Com Won";
 
         }else{
+            this.controlKotakKeterangan.style.display="block";
+            this.controlKeterangan.style.display ="block";
         console.log(angka);
         console.log(nilai);
         this.controlKeterangan.textContent="Draw";
@@ -105,60 +185,7 @@ class Game {
         }
 
     }
-    // tampilkanHasil(angka) {
-    //   if (this.giliran === "anda") {
-    //     this.mobilAnda.pindahPosisi(angka);
-    //   } else if (this.giliran === "robot") {
-    //     this.mobilRobot.pindahPosisi(angka);
-    //   }
-  
-    //   setTimeout(() => {
-    //     if (this.giliran === "anda" && this.mobilAnda.posisiSaatIni === 20) {
-    //       this.tampilkanPemenang();
-    //       return;
-    //     } else if (
-    //       this.giliran === "robot" &&
-    //       this.mobilRobot.posisiSaatIni === 20
-    //     ) {
-    //       this.tampilkanPemenang();
-    //       return;
-    //     }
-  
-    //     // Kode dibawah untuk mengubah giliran
-    //     if (this.giliran === "anda") {
-    //       this.giliran = "robot";
-  
-    //       this.controlKeterangan.textContent = "Giliran Robot";
-  
-    //       // Robot langsung mengacak angkanya
-    //       this.acakAngka();
-    //     } else if (this.giliran === "robot") {
-    //       this.giliran = "anda";
-  
-    //       this.controlKeterangan.textContent = "Giliran Anda";
-  
-    //       // Kembalikan control button seperti semula
-    //       this.controlButton.style.display = null;
-    //     }
-    //   }, 1000); // 1000 = 1 detik
-    // }
-  
-    // tampilkanPemenang() {
-    //   // Tampilkan kembali overlay
-    //   const overlay = document.querySelector("#overlay");
-    //   overlay.style.display = null;
-  
-    //   // Ubah text overlay
-    //   const overlayTitle = document.querySelector("#overlay .title");
-    //   if (this.giliran === "anda") {
-    //     overlayTitle.textContent = "Selamat, anda menang!";
-    //   } else if (this.giliran === "robot") {
-    //     overlayTitle.textContent = "Yah, anda kalah :(";
-    //   }
-  
-    //   const overlayButton = document.querySelector("#overlay button");
-    //   overlayButton.textContent = "Main lagi";
-    // }
+   
   }
 
   new Game ();
